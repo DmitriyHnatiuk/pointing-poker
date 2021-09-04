b8b26073aca30001dfbf9e366b484b2f4406a731
Dmytro Hnatiuk <Semon881@gmail.com>
2021-09-04 23:47:19 +0300

    refactor: change structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
 import store from 'redux/store';
+
+import 'assets/styles/global/reset.scss';
+
 import App from 'components/App';
 
 ReactDOM.render(
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,13 @@
-import { createStore } from 'redux';
+/* eslint-disable import/no-extraneous-dependencies */
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import reducer from 'redux/reducer/reducer';
 
-const store = createStore(reducer);
+const sagaMiddleware = createSagaMiddleware();
+const devTools = composeWithDevTools(applyMiddleware(sagaMiddleware));
+
+const store = createStore(reducer, devTools);
 
 export default store;
