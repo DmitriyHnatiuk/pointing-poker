9fb47c766c7cd597cbce6de9ee006fe7eee1b025
DmitriyHnatiuk <semon881@gmail.com>
2021-09-17 03:25:18 +0300

    feat: add connect to socket

--- src/pages/AdminLobby/GameStatus/index.tsx
+++ src/pages/AdminLobby/GameStatus/index.tsx
@@ -1,32 +1,61 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import { ways } from 'constants/constRouter';
+import { RootState } from 'redux/store';
+import { btn } from 'constants/commonComponents';
+
+import PlayerCard from 'components/common/PlayerCard/PlayerCard';
+import MyButton from 'components/common/MyButton/MyButton';
 import styles from './index.module.scss';
 
+const { HOME } = ways;
+const { START_GAME, CANCEL_GAME } = btn;
+
+const set = () => console.log('set-set'); // #
+
 const GameStatus: React.FC = (): JSX.Element => {
-	const planningTitle: any = useSelector<{ planningTitle: string }>(
-		(state) => state.planningTitle
+	const user = useSelector((state: RootState) => state.userReducer);
+	const planningTitle = useSelector(
+		(state: RootState) => state.gameSettings.planningTitle
 	);
+	const roomNumber = useSelector(
+		(state: RootState) => state.userReducer.roomNumber
+	);
+	const history = useHistory();
+	const toHome = () => history.push(HOME);
+
+	const changeLink = (event: React.ChangeEvent<HTMLInputElement>) =>
+		console.log(event.target.value); // #
+
 	return (
 		<>
 			<h1 className={styles.planningTitle}>{planningTitle}</h1>
 			<div className={styles.status}>
 				<span className={styles.titleStatus}>Scram master:</span>
-				{/* plateAdmin */}
+				<PlayerCard user={user} />
 			</div>
 			<div className={styles.gameStatus}>
 				<label htmlFor="link" className={styles.titleLink}>
 					<p>Link to lobby:</p>
-					<input className={styles.inputLink} type="text" name="link" />
+					<input
+						className={styles.inputLink}
+						type="text"
+						name="link"
+						value={roomNumber}
+						onChange={changeLink}
+					/>
 					<input className={styles.buttonCopy} type="submit" value="Copy" />
 				</label>
 				<div className={styles.buttonGame}>
-					<button className={styles.Start} type="button">
-						Start Game
-					</button>
-					<button className={styles.Cancel} type="button">
-						Cancel game
-					</button>
+					<MyButton onclick={set} value={START_GAME} />
+					<MyButton
+						onclick={toHome}
+						value={CANCEL_GAME}
+						style={styles.cancel}
+					/>
 				</div>
 			</div>
 		</>
--- src/pages/AdminLobby/index.tsx
+++ src/pages/AdminLobby/index.tsx
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import { setUserDataActionCreation } from 'redux/reducer/userReducer';
+import { RootState } from 'redux/store';
+import { URL } from 'constants/API';
 
 import GameStatus from './GameStatus';
 import Issues from './Issues';
 import Members from './Members';
 import AdminMenu from './AdminMenu';
 
-import styles from './styles.module.scss';
+import styles from './index.module.scss';
+
+const socket = io(URL, { autoConnect: false });
 
 const AdminLobby: React.FC = (): JSX.Element => {
+	const history = useHistory();
+	const user = useSelector<RootState>((state) => state.userReducer);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		socket.connect();
+
+		socket.emit(
+			'event://user connect without members data',
+			user,
+			(res: { status: string }) => {
+				const response = res.status
+					? console.log('done')
+					: console.log('error');
+				return response;
+			}
+		);
+
+		socket.on('event://your room name', (roomName) => {
+			console.log(roomName);
+			const obj = { roomNumber: roomName };
+			return dispatch(setUserDataActionCreation(obj));
+		});
+
+		socket.on('event://message', (ms, obj) => {
+			socket.emit('clientMessage', 'socketConnect');
+			console.log(ms, obj);
+		});
+	}, []);
+
+	socket.on('event://error', (err) => {
+		console.log(err);
+		history.push('/');
+	});
+
 	return (
 		<div className={styles.adminLobby}>
 			<GameStatus />
