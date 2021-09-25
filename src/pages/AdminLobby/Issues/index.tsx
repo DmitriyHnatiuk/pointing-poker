22ad11f884119265e149ca8ba0d5b71d8069938e
Nick Frolov <frolovnick0@gmail.com>
2021-09-25 20:39:40 +0300

    fear: add issue-component

--- src/pages/AdminLobby/Issues/index.tsx
+++ src/pages/AdminLobby/Issues/index.tsx
@@ -1,27 +1,47 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { getGame } from 'redux/reducer/selectors';
+import { Game } from 'redux/reducer/gameSettingReducer/types';
+import { addIssue } from 'redux/reducer/gameSettingReducer';
+
+import { useTypedSelector } from 'hooks/useTypedSelector';
+
+import IssueCard from 'components/common/IssueCard';
+
+import addIssueImage from 'assets/images/PlayingCard/add_card.svg';
 
 import styles from './index.module.scss';
 
 const Issues: React.FC = (): JSX.Element => {
-	const issues = 'hi';
+	const { issues } = useTypedSelector<Game>(getGame);
+
+	const dispatch = useDispatch();
+
+	const onAddIssue = () => {
+		dispatch(addIssue());
+	};
 
 	return (
-		<div id="issues" className={styles.issues}>
+		<div id="issues" className={styles.container}>
 			<h3 className={styles.title}>Issues:</h3>
-			<ul className={styles.issueList}>
-				<li className={styles.box}>
-					<h2 className={styles.nameIssues}>{issues}</h2>
-					<p>
-						<span className={styles.wastebasket}>🗑</span>
-						<span className={styles.pen}>✎</span>
-					</p>
-					<p className={styles.description}>Low profile</p>
-				</li>
-				<li className={styles.newOne}>
-					<h4 className={styles.nameOneNewIssue}>Crete new Issue</h4>
-					<span className={styles.plus}>+</span>
-				</li>
-			</ul>
+			<div className={styles.issues}>
+				{issues.map((issue) => (
+					<IssueCard issue={issue} key={issue.id} />
+				))}
+				<div className={styles.new}>
+					<div className={styles.newContent}>
+						<span>Crete new Issue</span>
+						<img
+							src={addIssueImage}
+							alt="add"
+							title="Add issue"
+							onClick={onAddIssue}
+							aria-hidden="true"
+						/>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
