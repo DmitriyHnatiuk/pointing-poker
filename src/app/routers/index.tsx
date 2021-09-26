76ef92571f25deef5e862984b065ce2f23001031
Sergey <46130311+SyleimanovSergey@users.noreply.github.com>
2021-09-26 22:29:16 +0600

    fix: fix path ChatPage

--- src/app/routers/index.tsx
+++ src/app/routers/index.tsx
@@ -13,7 +13,7 @@ import MainPage from 'pages/MainPage';
 import Error404 from 'pages/Error404';
 import AdminLobby from 'pages/AdminLobby';
 import TeamMembers from 'pages/TeamMembers';
-import PageChat from '../../pages/ChatPage';
+import PageChat from 'pages/ChatPage';
 
 const { HOME, ADMIN, USER, ERROR, CHAT } = ways;
 
